a1ca3d5950ffff4d86a4b865da7e5318339e08cd
Webzonedk <webzone@kpdesign.dk>
2022-04-26 08:56:54 +0200

    button and potMeter added, just writing to serial

--- Esp32/Esp32.ino
+++ Esp32/Esp32.ino
@@ -8,38 +8,105 @@
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 /////// WiFi Settings ///////
-const char* ssid = SECRET_SSID;
-const char* password = SECRET_PASS;
 
-// const char *ssid = SECRET_SSID;
-// const char *password = SECRET_PASS;
+const char *ssid = SECRET_SSID;
+const char *password = SECRET_PASS;
+
+// Difining the pins used for buttons
+const int missionSelectorButton = 34;
+const int potPin = 36;
+
+// Properties to keep data
+int temp = 0;
+int mission = 0;
+
+// Throttle properties
+int currentPotValue; // do not change
+int newPotValue;     // do not change
+float voltage = 0;   // do not change
+
+char serverAddress[] = "192.168.0.200"; // server address for the API at school
+int port = 8001;                        // Port number for the API
+
+// WiFiClient wifi;
+// HttpClient client = HTTPClient(wifi, serverAddress, port);
+int status = WL_IDLE_STATUS;
 
 void setup()
 {
 
     Serial.begin(115200);
-    delay(4000); // Delay needed before calling the WiFi.begin
+
+    pinMode(missionSelectorButton, INPUT); // Setting the pin to INPUT mode
+
+    // delay(4000); // Delay needed before calling the WiFi.begin
 
     WiFi.begin(ssid, password);
 
     while (WiFi.status() != WL_CONNECTED)
     { // Check for the connection
-        delay(1000);
-        Serial.println("Connecting to WiFi..");
+      // delay(1000);
+        Serial.print("Attempting to connect to Network named: ");
+        Serial.println(ssid); // print the network name (SSID);
     }
 
-    Serial.println("Connected to the WiFi network");
+    Serial.print("Connected to SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("with IP Address: ");
+    Serial.println(ip);
+
+    setPotValueStart();
+    printCurrentPotValue();
+    delay(4000);
 }
 
 void loop()
 {
-    if (WiFi.status() == WL_CONNECTED) // Check WiFi connection status
-    { 
-        Serial.println("Still connected to wifi");
-    }
-    else
+    missionSelector();
+
+    readPotMeterValue();
+
+    delay(50); // Delay for stability, as the potentiometer is not very stable in its readings
+}
+
+// Setting the starting value for the potentiometer
+void setPotValueStart()
+{
+    newPotValue = analogRead(potPin);
+    currentPotValue = newPotValue;
+}
+
+void printCurrentPotValue()
+{
+    Serial.print("PotValue: ");
+    Serial.print(newPotValue);
+    Serial.print("  -  Voltage: ");
+    Serial.println(voltage);
+}
+
+// read the input on analog pin potPin:
+void readPotMeterValue()
+{
+    newPotValue = analogRead(potPin);
+    if (newPotValue <= currentPotValue - 150 || newPotValue >= currentPotValue + 150)
     {
-        Serial.println("Wifi connection dropped");
+        currentPotValue = newPotValue;
+        voltage = (3.3 / 4095.0) * currentPotValue;
+
+        printCurrentPotValue();
     }
-    delay(2000);
+}
+
+//Taking input from the mission selector button
+void missionSelector()
+{
+    temp = digitalRead(missionSelectorButton);
+    if (temp == HIGH)
+    {
+        mission++;
+        Serial.println("Selector on");
+    }
 }
